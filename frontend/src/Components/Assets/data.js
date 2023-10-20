24b440555a9466d7b59a5b246a6773a4c4e5c3d7
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-10-20 18:05:36 +0200

    created test data

---
+++ frontend/src/Components/Assets/data.js
@@ -0,0 +1,49 @@
+export let data_products = [
+  {
+    id: 1,
+    name: "Product 1",
+    image: "https://picsum.photos/200",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    price: 9.99,
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    image: "https://picsum.photos/200",
+    description:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: 19.99,
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    image: "https://picsum.photos/200",
+    description:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    price: 29.99,
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    image: "https://picsum.photos/200",
+    description:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    price: 39.99,
+  },
+  {
+    id: 5,
+    name: "Product 5",
+    image: "https://picsum.photos/200",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    price: 49.99,
+  },
+  {
+    id: 6,
+    name: "Product 6",
+    image: "https://picsum.photos/200",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: 59.99,
+  },
+];
