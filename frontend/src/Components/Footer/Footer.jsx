709484e9249cdb6f386b867480c98222393eb68e
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-10-21 14:33:45 +0200

    created a simple footer with style

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        <strong>Email:</strong> Store@gmail.com{" "}
+      </p>
+      <p>
+        <strong>Phone:</strong> 123-456-7890
+      </p>
+      <p>
+        <strong>Address:</strong> 1234 Main St, City, State 12345
+      </p>
+    </div>
+  );
+};
