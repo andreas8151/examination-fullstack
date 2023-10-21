a42ca8054f37b1854a9ee1b833f15718d73825a0
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-10-21 14:32:29 +0200

    added banner and category to ShopCategory props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ import { ShopCategory } from "./Pages/ShopCategory";
 import { Product } from "./Pages/Product";
 import { Cart } from "./Pages/Cart";
 import { LoginSignup } from "./Pages/LoginSignup";
+import { Footer } from "./Components/Footer/Footer";
+import men_banner from "./Components/Assets/men.png";
+import woman_banner from "./Components/Assets/women.png";
+import kids_banner from "./Components/Assets/kids.png";
 
 function App() {
   return (
@@ -14,15 +18,25 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory category="men" />} />
-          <Route path="/womens" element={<ShopCategory category="woman" />} />
-          <Route path="/kids" element={<ShopCategory />} category="kid" />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="mens" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={woman_banner} category="womens" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kids_banner} category="kid" />}
+          />
           <Route path="/product" element={<Product />} />
           <Route path=":productId" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
         </Routes>
       </BrowserRouter>
+      <Footer />
     </div>
   );
 }
