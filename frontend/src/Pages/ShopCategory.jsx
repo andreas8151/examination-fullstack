d1064b3c10668ba3c8a85c6397dce133fce8b2cf
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-10-21 14:35:44 +0200

    created category function that renders out products

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import "./CSS/shopCategory.css";
+import { ShopContext } from "../Context/ShopContext";
+import { useContext } from "react";
 
-export const ShopCategory = () => {
-  return <div>ShopCategory</div>;
+export const ShopCategory = (props) => {
+  const { all_data } = useContext(ShopContext);
+  return (
+    <div>
+      <div className="shop-category">
+        <h1>{props.category}</h1>
+        <img src={props.banner} alt="banner" />
+      </div>
+      <div className="shop-category-products">
+        {all_data.map((product) => {
+          if (product.category === props.category) {
+            console.log(product);
+            return (
+              <div className="product-card" key={product.id}>
+                <img src={product.image} alt="product" />
+                <h3>{product.name}</h3>
+                <p>${product.price}</p>
+              </div>
+            );
+          }
+          return null;
+        })}
+      </div>
+    </div>
+  );
 };
