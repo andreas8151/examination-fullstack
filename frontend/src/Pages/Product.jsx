ecc8c7deefe0c6f8f29cb2365d085e5020c5b522
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-10-22 20:25:25 +0200

    added function for giving PRoductDisplay and RelatedPRoducts props value

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { ProductDisplay } from "../Components/ProductDisplay/ProductDisplay";
+import { RelatedProducts } from "../Components/RelatedProducts/RelatedProducts";
 
 export const Product = () => {
-  return <div>Product</div>;
+  const { all_data } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_data.find((item) => item.id == productId);
+
+  return (
+    <div>
+      <ProductDisplay product={product} />
+      <RelatedProducts category={product} />
+    </div>
+  );
 };
