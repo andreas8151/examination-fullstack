0f97a0087743767c91599d74ac530453622992dd
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-10-22 20:17:58 +0200

    updated Item with style and Link

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./item.css";
+import { Link } from "react-router-dom";
 
 export const Item = (props) => {
   return (
     <div className="item">
-      <img src={props.image} alt="" className="item-img" />
-      <p>{props.name}</p>
-      <p>Rs. {props.price}</p>
+      <Link to={`/product/${props.id}`}>
+        <img
+          src={props.image}
+          alt=""
+          className="item-img"
+          onClick={window.scrollTo(0, 0)}
+        />
+        <p>{props.name}</p>
+        <p>$ {props.price}</p>
+      </Link>
     </div>
   );
 };
