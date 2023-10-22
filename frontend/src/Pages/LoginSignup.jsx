f2c6c8684743559d9672194f9edf55864e7b8bd7
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-10-22 20:24:27 +0200

    created page for sign up

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import "./CSS/loginSignup.css";
+import store from "../Components/Assets/store.png";
 
 export const LoginSignup = () => {
-  return <div>LoginSignup</div>;
+  return (
+    <div className="loginSignup">
+      <div className="signIn-container">
+        <img src={store} alt="login" />
+        <div className="loginSignup-container">
+          <h1>Sing up</h1>
+          <form>
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Continue</button>
+            <p className="login"></p>
+
+            <div className="loginSignup-agree">
+              <p>
+                Already have an account? <a href="/">Login</a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
