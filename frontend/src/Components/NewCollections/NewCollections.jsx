4d4679f44b4d1ad7cdc9be63ee7b60031c022e52
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-10-22 20:20:10 +0200

    added Item.jsx to NewCollections

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./newCollections.css";
 import { data_products } from "../Assets/data.js";
+import { Item } from "../Item/Item.jsx";
 
 export const NewCollections = () => {
   return (
-    <div className="new-collections">
-      <h1>New Collections</h1>
+    <div className="new-collections" id="new-collections">
+      <h1>NEW COLLECTION</h1>
       <hr />
       <div className="new-collections-items">
         {data_products.map((product) => {
           return (
-            <div className="new-collections-item">
-              <img src={product.image} alt="" className="collection-img" />
-              <p>{product.name}</p>
-              <p>$ {product.price}</p>
-            </div>
+            <Item
+              key={product.id}
+              id={product.id}
+              image={product.image}
+              name={product.name}
+              price={product.price}
+            />
           );
         })}
       </div>
