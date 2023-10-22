3fab89d1f4c5229e352c428fc1a5aaed63483e8f
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-10-22 20:23:42 +0200

    added jsx component CartItems to cart

--- frontend/src/Pages/Cart.jsx
+++ frontend/src/Pages/Cart.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { CartItems } from "../Components/CartItems/CartItems";
+import "./CSS/cart.css";
 
 export const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div>
+      <CartItems />
+    </div>
+  );
 };
