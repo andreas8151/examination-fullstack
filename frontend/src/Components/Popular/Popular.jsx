aa58648f6fb94806e879e6a427dc99f87bba4690
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-10-22 20:20:48 +0200

    added Item.jsx to popular

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./popular.css";
 import { data_products } from "../Assets/data.js";
 import popular from "../Assets/popular.jpg";
+import { Item } from "../Item/Item.jsx";
 
 export const Popular = () => {
   return (
@@ -9,16 +10,15 @@ export const Popular = () => {
       <img src={popular} alt="" className="popular-img" />
 
       <div className="popular-items">
-        {data_products.map((product) => {
-          return (
-            <div className="popular-item">
-              <img src={product.image} alt="" className="popular-item-img" />
-              <p>{product.name}</p>
-
-              <p>$ {product.price}</p>
-            </div>
-          );
-        })}
+        {data_products.map((item) => (
+          <Item
+            key={item.id}
+            id={item.id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
       </div>
     </div>
   );
