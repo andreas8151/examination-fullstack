ab8522519b162e85897c1823dd4b26429f3d95c5
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-10-23 17:07:17 +0200

    created validation for product

---
+++ backend/src/controllers/productControllers/validation/addProductValidation.js
@@ -0,0 +1,24 @@
+const joi = require("joi");
+
+const productScheme = joi.object({
+  name: joi.string().min(3).max(30).required(),
+  image: joi.string().min(3).max(100).required(),
+  description: joi.string().min(3).max(255).required(),
+  price: joi.number().min(1).required(),
+  category: joi.string().min(3).max(30).required(),
+});
+
+// Validation functions
+function addProductValidation({ name, image, description, price, category }) {
+  const result = productScheme.validate({
+    name,
+    image,
+    description,
+    price,
+    category,
+  });
+
+  return result;
+}
+
+module.exports = { addProductValidation };
