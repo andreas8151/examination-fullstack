ea53daa7670f3955f65f1c107281a42b7322b1f0
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-10-23 18:06:47 +0200

    updated text in component

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -8,19 +8,12 @@ export const LoginSignup = () => {
       <div className="signIn-container">
         <img src={store} alt="login" />
         <div className="loginSignup-container">
-          <h1>Sing up</h1>
+          <h1>Admin-login</h1>
           <form>
-            <input type="text" placeholder="Name" />
             <input type="text" placeholder="Email" />
             <input type="password" placeholder="Password" />
             <button>Continue</button>
             <p className="login"></p>
-
-            <div className="loginSignup-agree">
-              <p>
-                Already have an account? <a href="/">Login</a>
-              </p>
-            </div>
           </form>
         </div>
       </div>
