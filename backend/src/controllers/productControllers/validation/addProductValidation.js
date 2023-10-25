184deeb25195e5a8786695899684736592115b92
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-10-25 18:26:28 +0200

    updated joi schema category

--- backend/src/controllers/productControllers/validation/addProductValidation.js
+++ backend/src/controllers/productControllers/validation/addProductValidation.js
@@ -2,10 +2,15 @@ const joi = require("joi");
 
 const productScheme = joi.object({
   name: joi.string().min(3).max(30).required(),
-  image: joi.string().min(3).max(100).required(),
+  image: joi.string().min(3).required(),
   description: joi.string().min(3).max(255).required(),
   price: joi.number().min(1).required(),
-  category: joi.string().min(3).max(30).required(),
+  category: joi
+    .string()
+    .valid("mens", "womens", "kid")
+    .min(3)
+    .max(30)
+    .required(),
 });
 
 function addProductValidation({ name, image, description, price, category }) {
