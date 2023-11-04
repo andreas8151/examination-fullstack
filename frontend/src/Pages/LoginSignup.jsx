0e34bdb858c993b36cc3f28db796a8907aed4acd
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-11-04 16:17:15 +0100

    updated component

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,19 +1,55 @@
 import React from "react";
-import "./CSS/loginSignup.css";
 import store from "../Components/Assets/store.png";
+import { useState } from "react";
+import "./CSS/loginSignup.css";
 
 export const LoginSignup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:5050/admin/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    if (response.ok) {
+      console.log("Login successful");
+      window.location.href = "/admin";
+    } else {
+      const errorMessage = await response.text();
+      setMessage(errorMessage);
+    }
+  };
+
   return (
     <div className="loginSignup">
       <div className="signIn-container">
         <img src={store} alt="login" />
         <div className="loginSignup-container">
           <h1>Admin-login</h1>
-          <form>
-            <input type="text" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+          <form onSubmit={submitHandler}>
+            <input
+              type="text"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <button>Continue</button>
-            <p className="login"></p>
+            <p>{message}</p>
           </form>
         </div>
       </div>
