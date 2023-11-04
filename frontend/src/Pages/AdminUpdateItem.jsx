5e2bdaddc0ff1974955332ccaafd344399132c5b
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-11-04 16:16:24 +0100

    created adminUpdateItem component

---
+++ frontend/src/Pages/AdminUpdateItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { UpdateItem } from "../Components/UpdateItem/UpdateItem";
+import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+
+export const AdminUpdateItem = () => {
+  const { all_data } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_data.find((item) => item.id == productId);
+
+  return (
+    <div>
+      <UpdateItem product={product} />
+    </div>
+  );
+};
