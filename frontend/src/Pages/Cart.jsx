9e0dfbf8fb50df35e9e73d4b8c7777131f0811c2
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-11-04 16:16:53 +0100

    updated component

--- frontend/src/Pages/Cart.jsx
+++ frontend/src/Pages/Cart.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { CartItems } from "../Components/CartItems/CartItems";
+import { CheckoutForm } from "../Components/CheckoutForm/CheckoutForm";
+import { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+
 import "./CSS/cart.css";
 
 export const Cart = () => {
+  const { all_data, cart, removeFromCart, addToCart, emptyCart } =
+    useContext(ShopContext);
+
   return (
     <div>
-      <CartItems />
+      <CartItems
+        cart={cart}
+        all_data={all_data}
+        removeFromCart={removeFromCart}
+        addToCart={addToCart}
+        emptyCart={emptyCart}
+      />
+      <CheckoutForm cart={cart} all_data={all_data} />
     </div>
   );
 };
