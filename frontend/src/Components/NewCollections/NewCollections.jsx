a418c2b68aae38366bd464299d6d403b7d044be7
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-11-04 16:13:01 +0100

    updated component

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./newCollections.css";
-import { data_products } from "../Assets/data.js";
 import { Item } from "../Item/Item.jsx";
+import { ShopContext } from "../../Context/ShopContext";
 
 export const NewCollections = () => {
+  const { all_data } = useContext(ShopContext);
+
   return (
     <div className="new-collections" id="new-collections">
       <h1>NEW COLLECTION</h1>
       <hr />
       <div className="new-collections-items">
-        {data_products.map((product) => {
+        {all_data.map((product) => {
           return (
             <Item
               key={product.id}
